7acd88f9d0eaddf73f095d17a1ca3d688ce8ed6f
Marie Ch <zima04@mail.ru>
2025-05-22 00:27:43 +0300

    15 урок update

---
+++ src/test/java/OthersTests.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class OthersTests extends TestBase {
+
+    @Test
+    void updatePutTest() {
+        String authData = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"))
+        ;
+    }
+
+    @Test
+    void NotUpdatePutTest() {
+        String authData = "%}";
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400);
+    }
+    @Test
+    void UpdatePatchTest() {
+        String authData = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void checkIdUserWithStatusGetTest() {
+        given()
+                .log().uri()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2));
+    }
+
+    @Test
+    void deleteTest() {
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .log().uri()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+}
